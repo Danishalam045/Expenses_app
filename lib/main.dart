659d86f12e68805d913e31b22cb67da0ca4b98bc
Danishalam045 <Danish_Alam@Danish_Alam_045.(none)>
2024-06-20 01:11:52 +0530

    Adding themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:fourth_project/widgets/expenses.dart';
 
+var kSchemeColor = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
         useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252),
+        colorScheme: kSchemeColor,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kSchemeColor.onPrimaryContainer,
+          foregroundColor: kSchemeColor.primaryContainer,
+        ),
       ),
       home: const Expenses(),
     ),
